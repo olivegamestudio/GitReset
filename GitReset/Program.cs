e114e859ac9c7a1488e38444a316fd26cee40724
Simon Jefferies <simon@olivegame.studio>
2021-01-22 11:22:16 +0000

    Imported.

---
+++ GitReset/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace GitReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] directories = Directory.GetDirectories(args[0]);
+
+            foreach (string directory in directories)
+            {
+                Console.WriteLine("Updating: " + directory);
+                Directory.SetCurrentDirectory(directory);
+                GitCheckout();
+                GitPull();
+            }
+        }
+
+        static void GitCheckout()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Program Files\\Git\\bin\\git.exe";
+            p.StartInfo.Arguments = "checkout master";
+            p.StartInfo.UseShellExecute = false;
+            p.OutputDataReceived += delegate (object sender, DataReceivedEventArgs eventArgs) { Console.WriteLine(eventArgs.Data); };
+            p.Start();
+            p.WaitForExit();
+        }
+
+        static void GitPull()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Program Files\\Git\\bin\\git.exe";
+            p.StartInfo.Arguments = "pull --progress -v --no-rebase --tags --prune \"origin\"";
+            p.StartInfo.UseShellExecute = false;
+            p.OutputDataReceived += delegate (object sender, DataReceivedEventArgs eventArgs) { Console.WriteLine(eventArgs.Data); };
+            p.Start();
+            p.WaitForExit();
+        }
+    }
+}
