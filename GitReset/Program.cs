fb0f7d85383547abac260aa1b373f1026a24b662
Simon Jefferies <simon@olivegame.studio>
2021-01-22 17:16:29 +0000

    Added support for -s and -b switches.

--- GitReset/Program.cs
+++ GitReset/Program.cs
@@ -6,24 +6,49 @@ namespace GitReset
 {
     class Program
     {
+        static string MainBranch = "master";
+
         static void Main(string[] args)
         {
-            string[] directories = Directory.GetDirectories(args[0]);
+            string path = Directory.GetCurrentDirectory();
 
+            foreach (string argument in args)
+            {
+                if (argument.StartsWith("-s="))
+                {
+                    path = argument.Substring(3);
+                    continue;
+                }
+
+                if (argument.StartsWith("-b="))
+                {
+                    MainBranch = argument.Substring(3);
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"GitReset being performed on: {path}");
+            string[] directories = Directory.GetDirectories(path);
+            
             foreach (string directory in directories)
             {
-                Console.WriteLine("Updating: " + directory);
+                Console.WriteLine($"Updating: {directory}");
                 Directory.SetCurrentDirectory(directory);
                 GitCheckout();
                 GitPull();
             }
         }
 
+        static string GetGitExecutablePath()
+        {
+            return "C:\\Program Files\\Git\\bin\\git.exe";
+        }
+
         static void GitCheckout()
         {
             Process p = new Process();
-            p.StartInfo.FileName = "C:\\Program Files\\Git\\bin\\git.exe";
-            p.StartInfo.Arguments = "checkout master";
+            p.StartInfo.FileName = GetGitExecutablePath();
+            p.StartInfo.Arguments = $"checkout {MainBranch}";
             p.StartInfo.UseShellExecute = false;
             p.OutputDataReceived += delegate (object sender, DataReceivedEventArgs eventArgs) { Console.WriteLine(eventArgs.Data); };
             p.Start();
@@ -33,7 +58,7 @@ namespace GitReset
         static void GitPull()
         {
             Process p = new Process();
-            p.StartInfo.FileName = "C:\\Program Files\\Git\\bin\\git.exe";
+            p.StartInfo.FileName = GetGitExecutablePath();
             p.StartInfo.Arguments = "pull --progress -v --no-rebase --tags --prune \"origin\"";
             p.StartInfo.UseShellExecute = false;
             p.OutputDataReceived += delegate (object sender, DataReceivedEventArgs eventArgs) { Console.WriteLine(eventArgs.Data); };
